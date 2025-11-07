0c2b6e55016715006cb046b1291e9d8ee8c4386b
husnainahmed971 <husnainahmed971@gmail.com>
2025-11-07 16:20:20 +0500

    first commit

---
+++ PythonDFS/PythonDFS/extras/Hashing.py
@@ -0,0 +1,9 @@
+# Hashes a command-line given password
+
+import hashlib 
+
+password = input('Enter password: ')
+hash=hashlib.md5()
+hash.update(password.encode())
+password = hash.hexdigest()
+print(password)
